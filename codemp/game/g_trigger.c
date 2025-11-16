07d9600f66afe173c89e8524f98d556e342a4cdd
Slash <164894678+slash-fr@users.noreply.github.com>
2025-11-16 18:26:27 +0100

    Bmodel ("model") is now optional for triggers

--- codemp/game/g_trigger.c
+++ codemp/game/g_trigger.c
@@ -30,7 +30,11 @@ void InitTrigger( gentity_t *self ) {
 	if (!VectorCompare (self->s.angles, vec3_origin))
 		G_SetMovedir (self->s.angles, self->movedir);
 
-	trap->SetBrushModel( (sharedEntity_t *)self, self->model );
+	if (self->model && self->model[0] == '*') // Alnico mod: Bmodel is optional now
+	{
+		trap->SetBrushModel( (sharedEntity_t *)self, self->model );
+	}
+
 	self->r.contents = CONTENTS_TRIGGER;		// replaces the -1 from trap->SetBrushModel
 	self->r.svFlags = SVF_NOCLIENT;
 
